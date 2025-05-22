8c0b127a82cabc37d4959ae1ad1f6d660f3e4320
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-05-22 23:48:34 +0100

    Add 0-javascript_is_amazing.js

---
+++ OneDrive/ALX Milestones/week-3/js-basics/hello.js
@@ -0,0 +1,32 @@
+/*console.log("Hello, World!")
+const name = "Aisha"
+console.log("Hello, " + name + "!")
+
+const name = "Aisha"
+
+console.log(name[4]);
+console.log(name.length);
+console.log(name.toUpperCase());
+
+let result = name.toLowerCase();
+console.log(result);
+
+let index = name.indexOf("i");
+console.log(index);*/
+
+let email = "aisha.mujahid.kabir@gmail.com";
+// let result = email.lastIndexOf("j");
+
+// let result = email.slice(5, 15);
+
+// let result2 = email.substring(5, 15);
+
+// let result = email.replace("aisha", "safiyyah");
+// console.log(result);
+
+let array = ["Aisha", "Mujahid", "Kabir"];
+//let result = array.join(" ");
+
+//let result = email.includes("me");
+let result = array.includes("baby");
+console.log(result);
