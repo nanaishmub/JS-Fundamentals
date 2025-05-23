62ded7f654421bb9cc293ad42a9cef6f700a49df
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-05-23 16:18:20 +0100

    Add arguments

---
+++ 2-arguments.js
@@ -0,0 +1,15 @@
+const argument = process.argv;
+
+if (argument.length < 3){
+    console.log("No argument");
+} else if (argument.length === 3) {
+    console.log("Argument Found");
+} else {
+    console.log("Arguments Found");
+};
+
+// console.log(argument[2]);
+
+
+//onsole.log(process.argv.length);
+
