d8d35771c5d38fa0a8af6ad51039bd48db5784ba
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-05-24 07:17:54 +0100

    Add square

--- 8-square.js
+++ 8-square.js
@@ -7,7 +7,7 @@ if (isNaN(rows) && isNaN(cols)){
     console.log("Missing size")
 } else {
 for (let i = 1; i <= rows; i++) {
-    let sqr = " ";
+    let sqr = "";
     for (let j = 1; j <= cols; j++){
         sqr += "X";
     }
