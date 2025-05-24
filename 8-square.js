c3768684d6ee5c9f2dc8b00695fdc5234be8d19e
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-05-24 07:08:04 +0100

    Add square

---
+++ 8-square.js
@@ -0,0 +1,16 @@
+const argument = process.argv;
+
+rows = parseInt(argument[2]);
+cols = parseInt(argument[2]);
+
+if (isNaN(rows) && isNaN(cols)){
+    console.log("Missing size")
+} else {
+for (let i = 1; i <= rows; i++) {
+    let sqr = " ";
+    for (let j = 1; j <= cols; j++){
+        sqr += "x";
+    }
+    console.log(sqr);
+    };
+};
