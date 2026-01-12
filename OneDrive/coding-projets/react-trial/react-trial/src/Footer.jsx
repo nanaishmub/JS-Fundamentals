beded76e07f456ee6f6f290dd97da5280cabcd7b
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2026-01-12 10:37:36 +0100

    .

---
+++ OneDrive/coding-projets/react-trial/react-trial/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} AMK</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ OneDrive/coding-projets/react-trial/react-trial/src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header(){
+    return(
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
