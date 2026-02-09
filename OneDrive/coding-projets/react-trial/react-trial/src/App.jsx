c05883e4f4ead214815e0ae4818ed0104c2a28f6
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2026-02-09 22:16:56 +0100

    initial commit, added navbar

--- OneDrive/coding-projets/react-trial/react-trial/src/App.jsx
+++ OneDrive/coding-projets/react-trial/react-trial/src/App.jsx
@@ -1,13 +1,16 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+import {useState} from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
+
+    const [theme, setTheme] = useState('light');
+
     return(
-      <>
-      <Header/>
-      <Footer/>
-      </>
+        <div className='dark:bg-black relative'>
+            <Navbar theme={theme} setTheme={setTheme}/>
+        </div>
     )
 }
+    
 
 export default App
